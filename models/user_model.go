9dbd6a1c4f40ea012a8a3c0c62d30352b1fd71ee
metindokmen <kindmemento@gmail.com>
2023-09-05 04:18:43 +0300

    implement RegistrationInput and User structs

--- models/user_model.go
+++ models/user_model.go
@@ -6,13 +6,22 @@ import (
 	"gorm.io/gorm"
 )
 
+type RegistrationInput struct {
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
 type User struct {
 	gorm.Model
-	username      string `gorm:"type:varchar(55);unique;not null"`
-	first_name    string `gorm:"type:varchar(55);not null"`
-	last_name     string `gorm:"type:varchar(55);not null"`
-	email         string `gorm:"type:varchar(100);unique;not null"`
-	password_hash string `gorm:"type:varchar(255);not null"`
-	created_at    time.Time
-	updated_at    time.Time
+	Username     string         `gorm:"type:varchar(55);unique;not null"`
+	FirstName    string         `gorm:"type:varchar(55);not null"`
+	LastName     string         `gorm:"type:varchar(55);not null"`
+	Email        string         `gorm:"type:varchar(100);unique;not null"`
+	PasswordHash string         `gorm:"type:varchar(255);not null"`
+	CreatedAt    time.Time      `gorm:"column:created_at"`
+	UpdatedAt    time.Time      `gorm:"column:updated_at"`
+	DeletedAt    gorm.DeletedAt `gorm:"index"`
 }
